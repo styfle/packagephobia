a9ef4cc826ba3773940cfa1a80bec089476ce7de
Steven <steven@ceriously.com>
2018-03-20 20:18:02 -0400

    Fix bugs that typescript caught 🎉

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-const { createServer } = require('http');
-const { parse } = require('url');
-const { lstatSync, readdirSync } = require('fs');
-const { join } = require('path');
-const promisify = require('util').promisify;
-const exec = promisify(require('child_process').exec);
-
+import { createServer } from 'http';
+import { parse } from 'url';
+import { lstatSync, readdirSync } from 'fs';
+import { join } from 'path';
+import { promisify } from 'util';
+import * as child_process from 'child_process'; 
+const exec = promisify(child_process.exec);
 const { TMPDIR='/tmp', PORT=3107 } = process.env;
 
+function getDirSize(root: string, size=0): number {
+    const stats = lstatSync(root);
 
-function getDirSize(root, size=0) {
-    const itemStats = lstatSync(root);
-
-    if (!itemStats.isDirectory()) {
-        return itemStats.size + size;
+    if (!stats.isDirectory()) {
+        return stats.size + size;
     }
 
     return readdirSync(root)
@@ -20,17 +19,17 @@ function getDirSize(root, size=0) {
         .reduce((acc, num) => acc + num, size);
 }
 
-async function getPackageSize(package) {
-    console.log(`getPackageSize(${package})`);
+async function getPackageSize(pkg: string) {
+    console.log(`getPackageSize(${pkg})`);
     const tmpPackage = 'tmp-package' + Math.random();
     const pkgDir = join(TMPDIR, tmpPackage);
     const nodeModules = join(pkgDir, 'node_modules');
     console.log({ TMPDIR, tmpPackage, pkgDir, nodeModules });
     await exec(`mkdir ${tmpPackage}`, { cwd: TMPDIR });
     await exec(`npm init -y`, { cwd: pkgDir });
-    await exec(`npm install --save ${package}`, { cwd: pkgDir });
+    await exec(`npm install --save ${pkg}`, { cwd: pkgDir });
     const size = getDirSize(nodeModules);
-    console.log(`Size of ${package}: ${size} bytes`);
+    console.log(`Size of ${pkg}: ${size} bytes`);
     await exec(`rm -rf ${tmpPackage}`, { cwd: TMPDIR });
     return size;
 }
@@ -38,8 +37,8 @@ async function getPackageSize(package) {
 createServer(async (req, res) => {
     let { httpVersion, method, url } = req;
     console.log(`${httpVersion} ${method} ${url}`);
-    const { query } = parse(req.url, true);
-    if (query && query.package) {
+    const { query } = parse(req.url || '', true);
+    if (query && query.package && typeof query.package === 'string') {
         const size = await getPackageSize(query.package);
         res.writeHead(200, {'Content-Type': 'application/json'});
         res.end(JSON.stringify({ size }));
