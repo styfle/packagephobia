7a577a6ec33bfd30513a859f1cd59380b0eefe33
Steven <steven@ceriously.com>
2018-03-29 19:53:56 -0400

    Add BarGraph component

--- pages/result.tsx
+++ pages/result.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import 'isomorphic-unfetch';
-import { parsePackageString, getReadableFileSize } from '../src/utils';
+import { parsePackageString, getReadableFileSize } from '../utils/index';
+import BarGraph from '../components/BarGraph';
 
 
 interface Props {
@@ -48,9 +49,43 @@ export default class ResultPage extends React.Component<Props, State> {
             alignItems: 'center',
         };
 
+        const readings = [
+            {
+                version: '1.0.0',
+                size: 200124,
+                gzip: 123541,
+                disabled: false,
+            },
+            {
+                version: '1.0.1',
+                size: 200224,
+                gzip: 123641,
+                disabled: false,
+            },
+            {
+                version: '1.0.2',
+                size: 200524,
+                gzip: 123941,
+                disabled: false,
+            },
+            {
+                version: '1.0.3',
+                size: 200824,
+                gzip: 124041,
+                disabled: false,
+            },
+            {
+                version: '1.0.4',
+                size: 200924,
+                gzip: 124241,
+                disabled: false,
+            },
+        ];
+
         return (<div style={container}>
             <h1>{pkgVersion.name}@{pkgVersion.version}</h1>
-            <p>Package {pkgVersion.name} is size {readableSize}</p>
+            <p>Package {pkgVersion.name} is {readableSize} after npm install</p>
+            <BarGraph readings={readings} onBarClick={(r) => console.log('you clicked ' + r.version)} />
         </div>);
     }
 }
--- utils/pkg-stats.ts
+++ utils/pkg-stats.ts
@@ -4,8 +4,7 @@ import { promisify } from 'util';
 import * as child_process from 'child_process'; 
 const exec = promisify(child_process.exec);
 
-// TODO: Can this be optimized
-// by changing sync to async?
+// TODO: Can this be optimized by changing sync to async?
 function getDirSize(root: string, size=0): number {
     const stats = lstatSync(root);
 
@@ -18,14 +17,20 @@ function getDirSize(root: string, size=0): number {
         .reduce((acc, num) => acc + num, size);
 }
 
-export async function getPackageSize(pkg: string, tmpDir='/tmp') {
+export async function getPackageSize(pkgName: string, pkgVersion: string, tmpDir='/tmp'): Promise<PkgSize> {
     const tmpPackage = 'tmp-package' + Math.random();
     const pkgDir = join(tmpDir, tmpPackage);
     const nodeModules = join(pkgDir, 'node_modules');
     await exec(`mkdir ${tmpPackage}`, { cwd: tmpDir });
     await exec(`npm init -y`, { cwd: pkgDir });
-    await exec(`npm install --save ${pkg}`, { cwd: pkgDir });
-    const size = getDirSize(nodeModules);
+    await exec(`npm install --save ${pkgName}@${pkgVersion}`, { cwd: pkgDir });
+    const installSize = getDirSize(nodeModules);
+    const publishSize = getDirSize(join(nodeModules, pkgName));
     await exec(`rm -rf ${tmpPackage}`, { cwd: tmpDir });
-    return size;
+    return { publishSize, installSize };
+}
+
+interface PkgSize {
+    publishSize: number;
+    installSize: number;
 }
