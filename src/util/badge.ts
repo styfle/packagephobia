319379ea226e86e3f82dc1c448cc8c4dcb536f9a
Steven <steven@ceriously.com>
2018-07-11 15:31:19 -0400

    Remove .svg suffix

--- src/util/badge.ts
+++ src/util/badge.ts
@@ -4,5 +4,5 @@ export function getBadgeUrl(pkgSize: PkgSize) {
     const { installSize } = pkgSize;
     const { readable } = getReadableFileSize(installSize);
     const color = getHexColor(installSize);
-    return `https://badgen.now.sh/badge/install%20size/${readable}/${color}.svg`;
+    return `https://badgen.now.sh/badge/install%20size/${readable}/${color}`;
 }
