80eb32deb792a5afc5fb1b55f0912ab2a7d40f44
Steven <steven@ceriously.com>
2019-03-03 19:04:13 -0500

    Add redirect to badgen.net proper

--- src/util/badge.ts
+++ src/util/badge.ts
@@ -1,10 +1,17 @@
 import { getReadableFileSize, getHexColor } from './npm-parser';
+import { pages, hostname } from '../util/constants';
 
-export function getBadgeUrl(pkgSize: PkgSize) {
-    const { installSize } = pkgSize;
-    const { pretty: readable } = getReadableFileSize(installSize);
-    const color = getHexColor(installSize);
-    return `https://badgen.now.sh/badge/install%20size/${readable}/${color}`;
+export function getBadgeUrl(pkgSize: PkgSize, isLatest: boolean) {
+    const { name, version } = pkgSize;
+    return isLatest
+        ? `https://badgen.net/packagephobia/install/${name}`
+        : `https://badgen.net/packagephobia/install/${name}@${version}`;
+}
+
+export function getBadgeMarkdown(pkgNameAndVersion: string) {
+    const badgePage = `https://${hostname}${pages.badge}?p=${pkgNameAndVersion}`;
+    const resultPage = `https://${hostname}${pages.result}?p=${pkgNameAndVersion}`;
+    return `[![install size](${badgePage})](${resultPage})`;
 }
 
 export function getApiResponseSize(bytes: number): ApiResponseSize {
