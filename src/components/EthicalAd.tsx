7c524ce3e7053045f7dba49875c4bf8e714616f3
Steven <steven@ceriously.com>
2020-07-12 20:10:24 -0400

    Change class to flat

--- src/components/EthicalAd.tsx
+++ src/components/EthicalAd.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 export default function EthicalAd() {
     return (
         <>
-            <div id="ethicalad" data-ea-publisher="packagephobia" data-ea-type="image"></div>
+            <div
+                id="ethicalad"
+                className="flat"
+                data-ea-publisher="packagephobia"
+                data-ea-type="image"
+            ></div>
             <script async src="https://media.ethicalads.io/media/client/ethicalads.min.js"></script>
         </>
     );
