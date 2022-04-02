1b99565ff377caff1882e28c5c384e5e424123bd
Steven <steven@ceriously.com>
2022-04-02 12:20:32 -0400

    Bump ioredis 5 (#926)
    * Bump ioredis 5
    
    * Add missing return

--- src/util/backend/db.ts
+++ src/util/backend/db.ts
@@ -19,54 +19,34 @@ client.on('error', err => {
     console.error('Redis error: ', err);
 });
 
-export function findAll(name: string) {
-    return new Promise<{ [key: string]: PkgSize }>((resolve, reject) => {
-        client.hgetall(name, (err, reply) => {
-            if (err) {
-                reject(err);
-            } else {
-                const obj: { [key: string]: PkgSize } = {};
-                for (let version in reply) {
-                    const payload: PkgSize = JSON.parse(reply[version] || '{}');
-                    payload.name = name;
-                    payload.version = version;
-                    obj[version] = payload;
-                }
-                resolve(obj);
-            }
-        });
-    });
+export async function findAll(name: string) {
+    const reply = await client.hgetall(name);
+    const obj: { [key: string]: PkgSize } = {};
+    for (let version in reply) {
+        const payload: PkgSize = JSON.parse(reply[version] || '{}');
+        payload.name = name;
+        payload.version = version;
+        obj[version] = payload;
+    }
+    return obj;
 }
 
-export function findOne(name: string, version: string) {
-    return new Promise<PkgSize | null>((resolve, reject) => {
-        client.hget(name, version, (err, reply) => {
-            if (err) {
-                reject(err);
-            } else {
-                if (!reply) {
-                    resolve(null);
-                } else {
-                    let record: PkgSize = JSON.parse(reply);
-                    record.name = name;
-                    record.version = version;
-                    resolve(record);
-                }
-            }
-        });
-    });
+export async function findOne(name: string, version: string) {
+    const reply = await client.hget(name, version);
+
+    if (!reply) {
+        return null;
+    }
+
+    let record: PkgSize = JSON.parse(reply);
+    record.name = name;
+    record.version = version;
+    return record;
 }
 
-export function insert(data: PkgSize) {
-    return new Promise<number>((resolve, reject) => {
-        const { name, version, ...payload } = data;
-        const value = JSON.stringify(payload);
-        client.hset(name, version, value, (err, reply) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(reply);
-            }
-        });
-    });
+export async function insert(data: PkgSize) {
+    const { name, version, ...payload } = data;
+    const value = JSON.stringify(payload);
+    const reply = await client.hset(name, version, value);
+    return reply;
 }
