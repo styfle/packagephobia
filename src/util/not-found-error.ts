4c523405b047ad76c50d35da101bd8b57737d1a4
Steven <steven@ceriously.com>
2022-12-11 19:05:10 -0500

    Improve error when package not found

---
+++ src/util/not-found-error.ts
@@ -0,0 +1,8 @@
+export class NotFoundError extends Error {
+    resource: string;
+    constructor({resource}: {resource: string }) {
+        super(`Resource "${resource}" was not found`);
+        this.name = 'NotFoundError';
+        this.resource = resource;
+    }
+}
